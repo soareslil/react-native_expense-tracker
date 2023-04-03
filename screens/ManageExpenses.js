e6db895575c8b547ed56329369be2854cd27f040
soareslil <lilianectd@gmail.com>
2023-04-03 11:14:35 -0300

    adding delete btn

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,5 +1,8 @@
 import { useLayoutEffect } from 'react';
-import { Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import IconButton from '../components/UI/IconButton';
+import { GlobalStyles } from '../constants/styles';
+
 function ManageExpenses({route, navigation}){
 
     const editedExpenseId = route.params?.expenseId;
@@ -11,9 +14,40 @@ function ManageExpenses({route, navigation}){
         });
     }, [navigation, isEditing]);
 
+    function deleteExpenseHandler(){
 
-    return <Text>manage expenses</Text>
+    }
+
+    return (
+        <View style={styles.container}>
+            { isEditing &&(
+                <View style={styles.deleteContainer}>
+                    <IconButton 
+                        icon="trash" 
+                        color={GlobalStyles.colors.error500} 
+                        size={36}
+                        onPress={deleteExpenseHandler}
+                    />
+                </View>
+           )}
+        </View>
+    )
 }
 
 export default ManageExpenses;
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary800
+    },
+    deleteContainer : { 
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: 'center'
+    },
+
+})
