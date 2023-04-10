f7e84f76a8acabcad03ff282a9c4349ed01727fd
soareslil <lilianectd@gmail.com>
2023-04-10 10:10:14 -0300

    fix bug

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import Input from "./Input";
+import Input from './Input';
 import Button from '../UI/Button';
 import { getFormattedDate } from '../../util/date';
-import { GlobalStyles } from "../../constants/styles";
+import { GlobalStyles } from '../../constants/styles';
 
-function ExpenseForm({onCancel, onSubmit, submitButtonLabel, defaultValues}) {
+function ExpenseForm({ submitButtonLabel, onCancel, onSubmit, defaultValues }) {
   const [inputs, setInputs] = useState({
-    amount:{ 
+    amount: {
       value: defaultValues ? defaultValues.amount.toString() : '',
       isValid: true,
     },
     date: {
-      value:defaultValues ? getFormattedDate(defaultValues.date) : '',
+      value: defaultValues ? getFormattedDate(defaultValues.date) : '',
       isValid: true,
     },
     description: {
@@ -26,12 +26,12 @@ function ExpenseForm({onCancel, onSubmit, submitButtonLabel, defaultValues}) {
     setInputs((curInputs) => {
       return {
         ...curInputs,
-        [inputIdentifier]: {value: enteredValue, isValid: true},
+        [inputIdentifier]: { value: enteredValue, isValid: true },
       };
     });
   }
 
-  function submitHandler(){
+  function submitHandler() {
     const expenseData = {
       amount: +inputs.amount.value,
       date: new Date(inputs.date.value),
@@ -39,34 +39,43 @@ function ExpenseForm({onCancel, onSubmit, submitButtonLabel, defaultValues}) {
     };
 
     const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
-    const dateIsValid = expenseData.date.toString() !== 'invalid date';
+    const dateIsValid = expenseData.date.toString() !== 'Invalid Date';
     const descriptionIsValid = expenseData.description.trim().length > 0;
 
-    if(!amountIsValid || !dateIsValid || !descriptionIsValid){
-      setInputs((curInputs)=> {
-        return{
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      // Alert.alert('Invalid input', 'Please check your input values');
+      setInputs((curInputs) => {
+        return {
           amount: { value: curInputs.amount.value, isValid: amountIsValid },
           date: { value: curInputs.date.value, isValid: dateIsValid },
-          description: { value: curInputs.description.value, isValid: descriptionIsValid },
-        }
+          description: {
+            value: curInputs.description.value,
+            isValid: descriptionIsValid,
+          },
+        };
       });
       return;
-    };
-    onSubmit(expenseData);
-  };
+    }
 
-  const formIsInvalid = !inputs.date.isValid || !inputs.amount.isValid || !inputs.description.isValid;
+    onSubmit(expenseData);
+  }
+
+  const formIsInvalid =
+    !inputs.amount.isValid ||
+    !inputs.date.isValid ||
+    !inputs.description.isValid;
+
   return (
     <View style={styles.form}>
-      <Text style={styles.title}>Your Expenses</Text>
-      <View style={styles.inputRow}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputsRow}>
         <Input
           style={styles.rowInput}
           label="Amount"
           invalid={!inputs.amount.isValid}
           textInputConfig={{
-            keyboardType: "decimal-pad",
-            onChangeText: inputChangedHandler.bind(this, "amount"),
+            keyboardType: 'decimal-pad',
+            onChangeText: inputChangedHandler.bind(this, 'amount'),
             value: inputs.amount.value,
           }}
         />
@@ -75,7 +84,7 @@ function ExpenseForm({onCancel, onSubmit, submitButtonLabel, defaultValues}) {
           label="Date"
           invalid={!inputs.date.isValid}
           textInputConfig={{
-            placeholder: "YYYY-MM-DD",
+            placeholder: 'YYYY-MM-DD',
             maxLength: 10,
             onChangeText: inputChangedHandler.bind(this, 'date'),
             value: inputs.date.value,
@@ -87,13 +96,19 @@ function ExpenseForm({onCancel, onSubmit, submitButtonLabel, defaultValues}) {
         invalid={!inputs.description.isValid}
         textInputConfig={{
           multiline: true,
-          onChangeText: inputChangedHandler.bind(this, "description"),
+          // autoCapitalize: 'none'
+          // autoCorrect: false // default is true
+          onChangeText: inputChangedHandler.bind(this, 'description'),
           value: inputs.description.value,
         }}
       />
-      {formIsInvalid && (<Text style={styles.errorText}>Invalid input values!</Text>)}
+      {formIsInvalid && (
+        <Text style={styles.errorText}>
+          Invalid input values - please check your entered data!
+        </Text>
+      )}
       <View style={styles.buttons}>
-        <Button style={styles.button} onPress={onCancel} mode="flat">
+        <Button style={styles.button} mode="flat" onPress={onCancel}>
           Cancel
         </Button>
         <Button style={styles.button} onPress={submitHandler}>
@@ -108,37 +123,34 @@ export default ExpenseForm;
 
 const styles = StyleSheet.create({
   form: {
-    marginTop: 100,
+    marginTop: 40,
   },
   title: {
     fontSize: 24,
-    fontWeight: "bold",
-    color: "white",
+    fontWeight: 'bold',
+    color: 'white',
     marginVertical: 24,
-    textAlign: "center",
+    textAlign: 'center',
   },
-  inputRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+  inputsRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   rowInput: {
     flex: 1,
   },
-  buttons:{
+  errorText: {
+    textAlign: 'center',
+    color: GlobalStyles.colors.error500,
+    margin: 8,
+  },
+  buttons: {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-},
-button: {
+  },
+  button: {
     minWidth: 120,
     marginHorizontal: 8,
-},
-errorText:{
-  color: GlobalStyles.colors.error500,
-  textAlign: 'center',
-  margin: 8,
-  padding: 30,
-  fontSize: 20,
-  fontWeight: 'bold',
-}
+  },
 });
